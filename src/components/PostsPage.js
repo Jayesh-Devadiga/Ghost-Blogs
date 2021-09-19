b356eeecb03c014f6c87487b765a9829323d8b5b
Jayesh <jd33.web@gmail.com>
2021-09-19 19:11:48 +0530

    Fetch data for post page

--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const PostsPage = () => {
+const PostsPage = ({ posts }) => {
+  const postsWithoutMetaDescription = posts.filter((post) => post.meta_description === null); //List of Posts without Meta Description
+  //   console.log(postsWithoutMetaDescription);
+  const postsWithLongMetaDescription = posts.filter((post) => post.meta_description && post.meta_description.length > 150); //Too long Meta Description, more than 150 char
+  //   console.log(postsWithLongMetaDescription);
+  const postsWithLongURL = posts.filter((post) => post.url.length > 100); //Too long URL, more than 100 chars
+  //   console.log(postsWithLongURL);
+  const postsWithoutImage = posts.filter((post) => post.feature_image === null);
+  // console.log(postsWithoutImage);
+  const shortPosts = posts.filter((post) => post.html.length < 250);
+  //   console.log(shortPosts);
+  const longPosts = posts.filter((post) => post.html.length > 1500);
+  //   console.log(longPosts);
+
   return <div>POSTS</div>;
 };
 
-export default PostsPage;
+const mapStateToProps = ({ data }) => {
+  return {
+    posts: data.posts,
+  };
+};
+
+export default connect(mapStateToProps)(PostsPage);
